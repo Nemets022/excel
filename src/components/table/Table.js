be564e64c70528fcd2ef4a7856ae4830ecc10e93
Nikita <Nemets022@gmail.com>
2020-06-28 19:20:16 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,23 @@
 import { ExcelComponent } from '../../core/ExelComponent'
 import { createTable } from './table.template'
+import { resizeHandler } from './table.resize'
+import { shoulResize } from './table.functions'
 
 export class Table extends ExcelComponent {
-	static className = 'excel__table'
+    static className = 'excel__table'
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown']
+        })
+    }
+
     toHTML() {
         return createTable()
+    }
+
+    onMousedown(event) {
+        if (shoulResize(event)) {
+            resizeHandler(this.$root, event)
+        }
     }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,31 @@ const CODES = {
     Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
     return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" data-col="${col}" contenteditable></div>
     `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
     return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+    ${col}
+    <div class="col-resize" data-resize="col"></div>
+    </div>
     `
 }
 
 function createRow(index, content) {
+    const resize = index ?
+     '<div class="row-resize" data-resize="row"></div>' :
+      ''
     return `
-    <div class="row">
-        <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+        <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+        </div>
         <div class="row-data">${content}</div>
     </div>
     `
@@ -28,7 +37,7 @@ function toChar(_, index) {
     return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowCount = 100) {
+export function createTable(rowCount = 20) {
     const colsCount = CODES.Z - CODES.A + 1
     const rows = []
 
@@ -45,7 +54,7 @@ export function createTable(rowCount = 100) {
             .fill('')
             .map(toCell)
             .join(' ')
-        rows.push(createRow(i+1, cells))
+        rows.push(createRow(i + 1, cells))
     }
 
     return rows.join(' ')
